595fba5e20f9767027dabc4e0d72dc2415544aa5
Egor Shpalov <gudel98@github.com>
2019-09-10 16:24:33 +0300

    Refactor Parser

--- lib/rubyqrpay/parser.rb
+++ lib/rubyqrpay/parser.rb
@@ -1,34 +1,35 @@
 module Rubyqrpay
   class Parser
+    NESTED_DATA_IDS = [ID_MERCHANT_INFORMATION_32,
+                       ID_MERCHANT_INFORMATION_33,
+                       ID_ADDITIONAL_DATA_FIELD,
+                       ID_MERCHANT_INFORMATION_LANGUAGE]
+
     class << self
       def parse_payload(payload)
-        parse_to_params(URI.unescape(payload))
+        parse_to_params(URI.unescape payload)
       end
 
       private
 
       def parse_to_params(payload)
-        params  = {}
-        index   = 0
+        {}.tap do |params|
+          index = 0
 
-        until payload[index + 4].nil?
-          id    = payload[index, 2]
-          size  = payload[index + 2, 2].to_i
-          value = payload[index + 4, size]
-          params[id] = value
-          index += 4 + size
-        end
+          until payload[index + 4].nil?
+            key = payload[index, 2]
+            size = payload[index + 2, 2].to_i
+            value = payload[index + 4, size]
 
-        params.map do |key, value|
-          if [ID_MERCHANT_INFORMATION_32,
-              ID_MERCHANT_INFORMATION_33,
-              ID_ADDITIONAL_DATA_FIELD,
-              ID_MERCHANT_INFORMATION_LANGUAGE].include? key
-            [key, parse_to_params(value)]
-          else
-            [key, value]
+            params[key] = if NESTED_DATA_IDS.include? key
+                            parse_to_params value
+                          else
+                            value
+                          end
+
+            index += 4 + size
           end
-        end.to_h
+        end
       end
     end
   end
